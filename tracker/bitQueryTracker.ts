2a7d81cd6362316b4177b1a42fd2ddd56e5db48f
vladirozen <75591466+vladiRozen@users.noreply.github.com>
2024-07-10 16:18:36 +1000

    feat: created unique array with count

--- tracker/bitQueryTracker.ts
+++ tracker/bitQueryTracker.ts
@@ -6,16 +6,34 @@ const addresses = [
 ]
 
 const track = async () => {
-    const trxs = await queryTransactions(addresses);
-    // const trxs = await queryTransactionsMock();
+    // const trxs = await queryTransactions(addresses);
+    const trxs = await queryTransactionsMock();
     console.log('trxs', JSON.stringify(trxs, null, 4));
 
     const { transfers } = trxs.data.solana;
-    if (transfers.length) {
-        const currencies = transfers.map((transfer: any) =>
-            ({name: transfer.currency.name, address: transfer.currency.address}));
-        console.log('currencies', currencies);
+
+    if (!transfers || !transfers.length) {
+        console.log('no transfers');
+        return;
     }
+
+    const currencies = transfers.map((transfer: any) =>
+        ({name: transfer.currency.name, address: transfer.currency.address})).filter(transfer => transfer.name !== "-")
+
+    const addressCount = currencies.reduce((acc, item) => {
+        acc[item.address] = (acc[item.address] || 0) + 1;
+        return acc;
+    }, {});
+
+    const uniqueItemsWithCount: any[] = [];
+    currencies.forEach(transfer => {
+        const existingItem = uniqueItemsWithCount.find(item => (item as any).address === transfer.address);
+        if (!existingItem) {
+            uniqueItemsWithCount.push({ ...transfer, count: addressCount[transfer.address] });
+        }
+    });
+
+    console.log('uniqueItemsWithCount', uniqueItemsWithCount);
 
 }
 
