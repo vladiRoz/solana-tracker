992bffeb3f4395df6a54692dc04b42fd41a2dd67
vladirozen <75591466+vladiRozen@users.noreply.github.com>
2024-07-11 17:34:46 +1000

    feat: updated query, fixes

--- tracker/QueryTransactions.ts
+++ tracker/QueryTransactions.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 import {subDays} from "date-fns/subDays";
 import {format} from "date-fns/format";
+import {response1} from "./Response1";
 
-// Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB - TUSD
 const query =
-    `query ($limit: Int!, $since: ISO8601DateTime, $receiver: [String!], $amount: Float!) {
+    `query ($limit: Int!, $since: ISO8601DateTime, $receiver: [String!], $amount: Float!, $ignoreCurrencies: [String!]) {
           solana(network: solana) {
             transfers(
               options: {limit: $limit, desc: "block.timestamp.iso8601"}
@@ -13,7 +13,7 @@ const query =
               transferType: {is: transfer}
               success: {is: true}
               amount: {gt: $amount}
-              currency: {notIn: ["Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB"]}
+              currency: {notIn: $ignoreCurrencies}
             ) {
               transaction {
                 signature
@@ -34,7 +34,7 @@ const query =
         }
     `;
 
-const getLastXDaysHoursFormatted = (days: number):string => {
+export const getLastXDaysHoursFormatted = (days: number):string => {
     const last24Hours = subDays(new Date(), days);
     return format(last24Hours, 'yyyy-MM-dd\'T\'HH:mm:00');
 }
@@ -45,10 +45,22 @@ export const queryTransactions = async (addresses: string[]) => {
     console.log('requesting trx since', since);
 
     const variables = {
-        "limit": 10,
+        "limit": 1000,
         "receiver": addresses,
         "amount": 1000,
         since,
+        "ignoreCurrencies": [
+            "Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB",
+            "So11111111111111111111111111111111111111112",
+            "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
+            "27G8MtK7VtTcCHkpASjSDdkWWYfoqT6ggEuKidVJidD4",
+            "JUPyiwrYJFskUPiHa7hkeR8VUtAeFoSYbKedZNsDvCN",
+            "6DNSN2BJsaPFdFFc1zP37kkeNe4Usc1Sqkzr9C9vPWcU",
+            "4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R",
+            "7vfCXTUXx5WJV5JADk17DUJ4ksgau7utNKj4b963voxs",
+            "7dHbWXmci3dT8UFYWYZweBLXgycu7Y3iL6trKn1Y7ARj",
+            "3NZ9JMVBmGAqocybic2c7LQCJScmgsAZ6vQqTDzcqmJh",
+            "mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So"],
     }
 
     const config = {
@@ -80,112 +92,112 @@ export const queryTransactions = async (addresses: string[]) => {
 }
 
 export const queryTransactionsMock = async () => {
-    const demoResponse =  {
-        "data": {
-            "solana": {
-                "transfers": [
-                    {
-                        "transaction": {
-                            "signature": "vHnsFtPDpnbyhfWgY6AsJeLnYA5rz2iiz4fSfcTZw3ePA5LQ171m7F3hxNLBQ5bVA9fvGy9KQeNia8zZSyJquNY"
-                        },
-                        "currency": {
-                            "name": "-",
-                            "symbol": "-",
-                            "address": "jWqoyb3itbujkhoiuuuMVsUHoCa1We1z5LF8voyN1Ra"
-                        },
-                        "any": "2024-07-09 13:40:30 UTC",
-                        "block": {
-                            "timestamp": {
-                                "iso8601": "2024-07-09T13:40:30Z"
-                            }
-                        }
-                    },
-                    {
-                        "transaction": {
-                            "signature": "3j8zVVdZLokSMEVnHQNEaBXsQNpx4M8Uerr3jgaQHD42ctqek6UH4ZMX9Y45GqJH2Zr1MK34cMkcR4zxXLRhKjPQ"
-                        },
-                        "currency": {
-                            "name": "Hawk Tuah",
-                            "symbol": "HAWKTUAH",
-                            "address": "4GFe6MBDorSy5bLbiUMrgETr6pZcjyfxMDm5ehSgpump"
-                        },
-                        "any": "2024-07-08 22:27:13 UTC",
-                        "block": {
-                            "timestamp": {
-                                "iso8601": "2024-07-08T22:27:13Z"
-                            }
-                        }
-                    },
-                    {
-                        "transaction": {
-                            "signature": "4Kx7SCzUKNHGNgpCegTqqAmQWmstER4my1SVXoRG2TCAvpXWcNQKXtNdYPZPQbdTgmbEDsrvQwcyADq7R6FtmGg7"
-                        },
-                        "currency": {
-                            "name": "-",
-                            "symbol": "-",
-                            "address": "B45QLZLZNwHo5ya5TQdjkE6A4xLCvNYvcRtjvFFZ9T72"
-                        },
-                        "any": "2024-07-08 14:54:51 UTC",
-                        "block": {
-                            "timestamp": {
-                                "iso8601": "2024-07-08T14:54:51Z"
-                            }
-                        }
-                    },
-                    {
-                        "transaction": {
-                            "signature": "58JQ5i4krB5s2LJDHQBvmsH8hemBrjgDuHyDwg6YoNSVp3Sq3DY84t6hHvS7rKb8gbgkYQY1XrcBo476ZCrQi4LR"
-                        },
-                        "currency": {
-                            "name": "Hawk Tuah",
-                            "symbol": "HAWKTUAH",
-                            "address": "4GFe6MBDorSy5bLbiUMrgETr6pZcjyfxMDm5ehSgpump"
-                        },
-                        "any": "2024-07-08 14:03:59 UTC",
-                        "block": {
-                            "timestamp": {
-                                "iso8601": "2024-07-08T14:03:59Z"
-                            }
-                        }
-                    },
-                    {
-                        "transaction": {
-                            "signature": "2dXZi3tMBYXcH8zQfkRpGZXn4ygYMAbjKCmUU7CB3gmde27unrX1mT5Ki1btfc4ACCk1PCBexGPXnoC2aHAncSPZ"
-                        },
-                        "currency": {
-                            "name": "-",
-                            "symbol": "-",
-                            "address": "6rThQfhir6PJXpgTBAY4K9dEhMZuLEajJmt6naQPNFau"
-                        },
-                        "any": "2024-07-07 11:28:43 UTC",
-                        "block": {
-                            "timestamp": {
-                                "iso8601": "2024-07-07T11:28:43Z"
-                            }
-                        }
-                    },
-                    {
-                        "transaction": {
-                            "signature": "9tBEnuBNsPcskhU5UhJcXZ2NfwdxqsGFginf1gsTUnoUiHFVYek4CkuothtzzHp2UM9bit68Fx8VNvV5Pu5TjyV"
-                        },
-                        "currency": {
-                            "name": "Pepe On Crack",
-                            "symbol": "PEPECRACK",
-                            "address": "VqF9cfQ6eKmWjB4C1LYnoVS8iAXevPwqmAdSjXEpump"
-                        },
-                        "any": "2024-07-07 03:57:01 UTC",
-                        "block": {
-                            "timestamp": {
-                                "iso8601": "2024-07-07T03:57:01Z"
-                            }
-                        }
-                    }
-                ]
-            }
-        }
-    };
+    // const demoResponse =  {
+    //     "data": {
+    //         "solana": {
+    //             "transfers": [
+    //                 {
+    //                     "transaction": {
+    //                         "signature": "vHnsFtPDpnbyhfWgY6AsJeLnYA5rz2iiz4fSfcTZw3ePA5LQ171m7F3hxNLBQ5bVA9fvGy9KQeNia8zZSyJquNY"
+    //                     },
+    //                     "currency": {
+    //                         "name": "-",
+    //                         "symbol": "-",
+    //                         "address": "jWqoyb3itbujkhoiuuuMVsUHoCa1We1z5LF8voyN1Ra"
+    //                     },
+    //                     "any": "2024-07-09 13:40:30 UTC",
+    //                     "block": {
+    //                         "timestamp": {
+    //                             "iso8601": "2024-07-09T13:40:30Z"
+    //                         }
+    //                     }
+    //                 },
+    //                 {
+    //                     "transaction": {
+    //                         "signature": "3j8zVVdZLokSMEVnHQNEaBXsQNpx4M8Uerr3jgaQHD42ctqek6UH4ZMX9Y45GqJH2Zr1MK34cMkcR4zxXLRhKjPQ"
+    //                     },
+    //                     "currency": {
+    //                         "name": "Hawk Tuah",
+    //                         "symbol": "HAWKTUAH",
+    //                         "address": "4GFe6MBDorSy5bLbiUMrgETr6pZcjyfxMDm5ehSgpump"
+    //                     },
+    //                     "any": "2024-07-08 22:27:13 UTC",
+    //                     "block": {
+    //                         "timestamp": {
+    //                             "iso8601": "2024-07-08T22:27:13Z"
+    //                         }
+    //                     }
+    //                 },
+    //                 {
+    //                     "transaction": {
+    //                         "signature": "4Kx7SCzUKNHGNgpCegTqqAmQWmstER4my1SVXoRG2TCAvpXWcNQKXtNdYPZPQbdTgmbEDsrvQwcyADq7R6FtmGg7"
+    //                     },
+    //                     "currency": {
+    //                         "name": "-",
+    //                         "symbol": "-",
+    //                         "address": "B45QLZLZNwHo5ya5TQdjkE6A4xLCvNYvcRtjvFFZ9T72"
+    //                     },
+    //                     "any": "2024-07-08 14:54:51 UTC",
+    //                     "block": {
+    //                         "timestamp": {
+    //                             "iso8601": "2024-07-08T14:54:51Z"
+    //                         }
+    //                     }
+    //                 },
+    //                 {
+    //                     "transaction": {
+    //                         "signature": "58JQ5i4krB5s2LJDHQBvmsH8hemBrjgDuHyDwg6YoNSVp3Sq3DY84t6hHvS7rKb8gbgkYQY1XrcBo476ZCrQi4LR"
+    //                     },
+    //                     "currency": {
+    //                         "name": "Hawk Tuah",
+    //                         "symbol": "HAWKTUAH",
+    //                         "address": "4GFe6MBDorSy5bLbiUMrgETr6pZcjyfxMDm5ehSgpump"
+    //                     },
+    //                     "any": "2024-07-08 14:03:59 UTC",
+    //                     "block": {
+    //                         "timestamp": {
+    //                             "iso8601": "2024-07-08T14:03:59Z"
+    //                         }
+    //                     }
+    //                 },
+    //                 {
+    //                     "transaction": {
+    //                         "signature": "2dXZi3tMBYXcH8zQfkRpGZXn4ygYMAbjKCmUU7CB3gmde27unrX1mT5Ki1btfc4ACCk1PCBexGPXnoC2aHAncSPZ"
+    //                     },
+    //                     "currency": {
+    //                         "name": "-",
+    //                         "symbol": "-",
+    //                         "address": "6rThQfhir6PJXpgTBAY4K9dEhMZuLEajJmt6naQPNFau"
+    //                     },
+    //                     "any": "2024-07-07 11:28:43 UTC",
+    //                     "block": {
+    //                         "timestamp": {
+    //                             "iso8601": "2024-07-07T11:28:43Z"
+    //                         }
+    //                     }
+    //                 },
+    //                 {
+    //                     "transaction": {
+    //                         "signature": "9tBEnuBNsPcskhU5UhJcXZ2NfwdxqsGFginf1gsTUnoUiHFVYek4CkuothtzzHp2UM9bit68Fx8VNvV5Pu5TjyV"
+    //                     },
+    //                     "currency": {
+    //                         "name": "Pepe On Crack",
+    //                         "symbol": "PEPECRACK",
+    //                         "address": "VqF9cfQ6eKmWjB4C1LYnoVS8iAXevPwqmAdSjXEpump"
+    //                     },
+    //                     "any": "2024-07-07 03:57:01 UTC",
+    //                     "block": {
+    //                         "timestamp": {
+    //                             "iso8601": "2024-07-07T03:57:01Z"
+    //                         }
+    //                     }
+    //                 }
+    //             ]
+    //         }
+    //     }
+    // };
 
-    return Promise.resolve(demoResponse);
+    return Promise.resolve({ data: response1 });
 }
 
 //
--- tracker/bitQueryTracker.ts
+++ tracker/bitQueryTracker.ts
@@ -1,14 +1,40 @@
-import {queryTransactions, queryTransactionsMock} from "./QueryTransactions";
+import {getLastXDaysHoursFormatted, queryTransactions, queryTransactionsMock} from "./QueryTransactions";
+
+// TODO is it possible to limit the number per address
+
+// "BwHKbKPt4bMvaAk5Qvc97euhTMNXL6qCtNwG9vse6CFQ",
+
+// limit 1000 = 32 points
+// limit 1000 = 8 points
+
+// iteration 15 addresses
+// 38 addresses total
+
+// 15 addresses * 2 * 8 * 30 = 7200 points
 
 const addresses = [
-    "BwHKbKPt4bMvaAk5Qvc97euhTMNXL6qCtNwG9vse6CFQ",
-    "CXGpHdDLrnzrKY1ukvgNKM8soZhzdL5Trx5bXQdeCHVL"
+    "CXGpHdDLrnzrKY1ukvgNKM8soZhzdL5Trx5bXQdeCHVL",
+    "8CGJPWdM7xZX5E2ZE7qw5TiNUgpLyUeXJAnHr5goezeD",
+    "DVMnHHwq9SNxj4Bhd8mim1AacXfPemu6WsK8ox7Vexhg",
+    "J7pMJuvFoNni6UxAavxZ26zH39gzjUS57NKNiAvezetj",
+    "H4yHKvbqethH5CFwc5DUjErHETWQcDBSyB2vkXbdGX7r",
+    "CRRf1zgmgbn5UfBp6sSsdjHAqCXFQRUzKaN2ni19Dgvk",
+    "j1oAbxxiDUWvoHxEDhWE7THLjEkDQW2cSHYn2vttxTF",
+    "356JHc8eZFYF1FKkcKtjNHfSQxhPzPtBEsgJEazS95Et",
+    "Du6KcjYDAsyBtMQTPd7ndhD6N7nb7TMoimgZtwvo3hDa",
+    "8yNf3DbJYoRqfxRHZTUduJPaBYQHpAjs7XSskpkrKxBg",
+    "BdwdDJuTw9tS2o6GEaCSESwKg1PXPASRumErm2VEMEjD",
+    "AG6YJF7iH1R5io1RtyjmKcyWjYPnmSP2zBuSRBawbNhG",
+    "3JNXb6rcKiswc1d1FfrBFV7TqSFKLGRFFoXMcxCW3VNY",
+    "3LDFRAVq88XTvUtUnz6xGWisB9ALhLFKfSxeaKzAFLX7",
+    "DLocg6wustgRS8sVd9grj3smUWmw3M6sSJrydgXWrAUC",
+    "FmhDonUB11n5WyRCDEpxnNJhXDVw1JX1XJSD72ssQFhB"
 ]
 
 const track = async () => {
-    // const trxs = await queryTransactions(addresses);
-    const trxs = await queryTransactionsMock();
-    console.log('trxs', JSON.stringify(trxs, null, 4));
+    const trxs = await queryTransactions(addresses);
+    // const trxs = await queryTransactionsMock();
+    // console.log('trxs', JSON.stringify(trxs, null, 4));
 
     const { transfers } = trxs.data.solana;
 
@@ -33,8 +59,13 @@ const track = async () => {
         }
     });
 
-    console.log('uniqueItemsWithCount', uniqueItemsWithCount);
+    const sortedCurrencies = uniqueItemsWithCount.sort((a, b) => (b as any).count - (a as any).count);
+
+    console.log('Top 10 Currencies', sortedCurrencies.slice(0, 10));
 
 }
 
 track();
+
+// const since = getLastXDaysHoursFormatted(2);
+// console.log('requesting trx since', since);
